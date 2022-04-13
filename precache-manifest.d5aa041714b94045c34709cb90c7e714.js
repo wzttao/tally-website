5cd9a98b643aec4107e789c067791d5188a27e71
wzt <1052987294@qq.com>
2022-04-13 18:20:24 +0800

    deploy

---
+++ precache-manifest.d5aa041714b94045c34709cb90c7e714.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4af1c6f5c46eb48628cb",
+    "url": "/tally-website/css/app.042190da.css"
+  },
+  {
+    "revision": "c29f57ffb853ae43058aa2dfd370bb49",
+    "url": "/tally-website/index.html"
+  },
+  {
+    "revision": "4af1c6f5c46eb48628cb",
+    "url": "/tally-website/js/app.fe57dda8.js"
+  },
+  {
+    "revision": "793bc2dee46212b99e04",
+    "url": "/tally-website/js/chunk-vendors.c6dde27d.js"
+  },
+  {
+    "revision": "3b3ac03b730001df064d3e92befbe4e5",
+    "url": "/tally-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/tally-website/robots.txt"
+  }
+]);
